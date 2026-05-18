c1cac16a67497ca5adcf3f34626a4c4eb19f5f13
AshilaMahdiyyah <147472711+AshilaMahdiyyah@users.noreply.github.com>
2026-05-18 17:17:53 +0700

    Add files via upload

---
+++ recommender.py
@@ -0,0 +1,64 @@
+
+import pandas as pd
+import pickle
+from sklearn.metrics.pairwise import cosine_similarity
+
+# LOAD DATA
+df_menu = pd.read_csv("df_menu_final.csv").reset_index(drop=True)
+
+# LOAD MODEL
+tfidf = pickle.load(open("tfidf.pkl", "rb"))
+item_profile = pickle.load(open("item_profile.pkl", "rb"))
+
+
+def get_rekomendasi(menu, flavor, price, dine, rating, top_n=10):
+
+    # user query
+    user_query = f"{menu} {flavor}"
+    user_vec = tfidf.transform([user_query])
+
+    df = df_menu.copy().reset_index(drop=True)
+
+    # similarity
+    sim = cosine_similarity(item_profile, user_vec).flatten()
+    df["similarity"] = sim
+
+    # feature engineering
+    df["rating_norm"] = df["avgRating"] / 5
+    df["price_match"] = (df["range_price"] == price).astype(int)
+
+    if dine == "both":
+        df["dine_match"] = 1
+    else:
+        df["dine_match"] = df["dine_option"].apply(
+            lambda x: 1 if x == dine or x == "both" else 0
+        )
+
+    df["combined"] = (
+        df["similarity"] +
+        df["rating_norm"] +
+        df["price_match"] +
+        df["dine_match"]
+    ) / 4
+
+    # filtering
+    df = df[df["avgRating"] >= rating]
+
+    if price:
+        df = df[df["range_price"] == price]
+
+    if dine != "both":
+        df = df[(df["dine_option"] == dine) | (df["dine_option"] == "both")]
+
+    # ranking
+    df = df.sort_values(
+        by=["combined", "similarity", "avgRating"],
+        ascending=False
+    )
+
+    df = df.drop_duplicates("nama_tempat")
+
+    df = df.head(top_n).reset_index(drop=True)
+    df["rank"] = df.index + 1
+
+    return df
