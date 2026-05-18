fd40242923548b8362f9ce9f4759a651d904cf52
AshilaMahdiyyah <147472711+AshilaMahdiyyah@users.noreply.github.com>
2026-05-18 15:43:52 +0700

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -6,7 +6,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 
 # LOAD DATA
-df_menu = pd.read_csv("dataset_with_menu_recommendation (1).csv")
+df_menu = pd.read_csv("dataset_with_menu_recommendation.csv")
 
 
 # LOAD MODEL
